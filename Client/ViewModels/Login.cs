21d080c4a2d0f67456df31d7ee28b6cbff1dd519
Marcin Tajsner <marcin.tajsner@gmail.com>
2020-11-20 00:51:16 +0100

    Próba tworzenia na MVVM

---
+++ Client/ViewModels/Login.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client.ViewModels {
+    public class Login {
+
+    }
+}
--- Client/Views/Login.xaml.cs
+++ Client/Views/Login.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -13,16 +11,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Client
-{
-    public partial class MainWindow : Window
-    {
-        public MainWindow() {
+namespace Client.Views {
+    public partial class Login : UserControl {
+        public Login() {
             InitializeComponent();
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e) {
-            DataContext = new ViewModels.Login();
         }
     }
 }
