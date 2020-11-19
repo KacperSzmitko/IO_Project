039d8056b26eeef5ca8acb10db246fe5b74497c5
KapelanJR <kacperszmitk@wp.pl>
2020-11-20 00:56:08 +0100

    Certificate generation

---
+++ ServerLibrary/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace ServerLibrary
+{
+    class Player
+    {
+        public IPAddress addres { get; set; }
+        public  int sessionId { get; set; }
+        public string name { get; set; }
+        
+        //Index to Games array
+        public int match_id { get; set; }
+
+        Player(IPAddress addres)
+        {
+            this.addres = addres;
+            this.sessionId = -1;
+            this.name = null;
+            this.match_id = -1;
+        }
+    }
+}
