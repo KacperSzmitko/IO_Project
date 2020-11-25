1e3e7d53862bf0a53f818c256a3d6fa7e5b04b2d
konrad.wodnicki <konrad.wodnicki@student.put.poznan.pl>
2020-11-25 19:46:53 +0100

    zmiany w gameplay.cs

--- ServerLibrary/Gameplay.cs
+++ ServerLibrary/Gameplay.cs
@@ -9,7 +9,6 @@ namespace ServerLibrary
     {
         public Player p1{get; set;}
         public Player p2{get; set;}
-        public string[] board { get; set; }
         public int p1Points { get; set; }
         public int p2Points { get; set; }
         public int pointsToWin { get; set; }
@@ -21,13 +20,13 @@ namespace ServerLibrary
         {
             this.p1 = p1;
             this.p2 = p2;
-            this.board = new string[boardSize];
+          
             this.pointsToWin = pointsToWin;
             p1Points = 0;
             p2Points = 0;
         }
 		// zapisanie danego ruchu i sprawdzenie co sie po nim stalo odbywa sie w metodzie move 
-		public string move(int pos, Player player)
+		public Boolean move(int pos, Player player)
 		{
 			string symbol = " ";
 			if (!p1Positions.Contains(pos) && !p2Positions.Contains(pos))
@@ -42,32 +41,134 @@ namespace ServerLibrary
 					symbol = "0";
 					p2Positions.Add(pos);
 				}
-				if (p1Positions.Contains(1) && p1Positions.Contains(2) && p1Positions.Contains(3)) return "p1 wins";
-				if (p1Positions.Contains(4) && p1Positions.Contains(5) && p1Positions.Contains(6)) return "p1 wins";
-				if (p1Positions.Contains(7) && p1Positions.Contains(8) && p1Positions.Contains(9)) return "p1 wins";
-				if (p1Positions.Contains(1) && p1Positions.Contains(4) && p1Positions.Contains(7)) return "p1 wins";
-				if (p1Positions.Contains(2) && p1Positions.Contains(5) && p1Positions.Contains(8)) return "p1 wins";
-				if (p1Positions.Contains(3) && p1Positions.Contains(6) && p1Positions.Contains(9)) return "p1 wins";
-				if (p1Positions.Contains(1) && p1Positions.Contains(5) && p1Positions.Contains(9)) return "p1 wins";
-				if (p1Positions.Contains(7) && p1Positions.Contains(5) && p1Positions.Contains(3)) return "p1 wins";
+				if (p1Positions.Contains(1) && p1Positions.Contains(2) && p1Positions.Contains(3)) {
+                    p1Points++;
+					p1Positions.Clear();
+					p2Positions.Clear();
+					return true; }
+				if (p1Positions.Contains(4) && p1Positions.Contains(5) && p1Positions.Contains(6))
+				{
+					p1Points++;
+					p1Positions.Clear();
+					p2Positions.Clear();
+					return true;
+				}
+				if (p1Positions.Contains(7) && p1Positions.Contains(8) && p1Positions.Contains(9))
+				{
+					p1Points++;
+					p1Positions.Clear();
+					p2Positions.Clear();
+					return true;
+				}
+				if (p1Positions.Contains(1) && p1Positions.Contains(4) && p1Positions.Contains(7))
+				{
+					p1Points++;
+					p1Positions.Clear();
+					p2Positions.Clear();
+					return true;
+				}
+				if (p1Positions.Contains(2) && p1Positions.Contains(5) && p1Positions.Contains(8))
+				{
+					p1Points++;
+					p1Positions.Clear();
+					p2Positions.Clear();
+					return true;
+				}
+				if (p1Positions.Contains(3) && p1Positions.Contains(6) && p1Positions.Contains(9))
+				{
+					p1Points++;
+					p1Positions.Clear();
+					p2Positions.Clear();
+					return true;
+				}
+				if (p1Positions.Contains(1) && p1Positions.Contains(5) && p1Positions.Contains(9))
+				{
+					p1Points++;
+					p1Positions.Clear();
+					p2Positions.Clear();
+					return true;
+				}
+				if (p1Positions.Contains(7) && p1Positions.Contains(5) && p1Positions.Contains(3))
+				{
+					p1Points++;
+					p1Positions.Clear();
+					p2Positions.Clear();
+					return true;
+				}
 
-				if (p2Positions.Contains(1) && p2Positions.Contains(2) && p2Positions.Contains(3)) return "p2 wins";
-				if (p2Positions.Contains(4) && p2Positions.Contains(5) && p2Positions.Contains(6)) return "p2 wins";
-				if (p2Positions.Contains(7) && p2Positions.Contains(8) && p2Positions.Contains(9)) return "p2 wins";
-				if (p2Positions.Contains(1) && p2Positions.Contains(4) && p2Positions.Contains(7)) return "p2 wins";
-				if (p2Positions.Contains(2) && p2Positions.Contains(5) && p2Positions.Contains(8)) return "p2 wins";
-				if (p2Positions.Contains(3) && p2Positions.Contains(6) && p2Positions.Contains(9)) return "p2 wins";
-				if (p2Positions.Contains(1) && p2Positions.Contains(5) && p2Positions.Contains(9)) return "p2 wins";
-				if (p2Positions.Contains(7) && p2Positions.Contains(5) && p2Positions.Contains(3)) return "p2 wins";
+
+
+				if (p2Positions.Contains(1) && p2Positions.Contains(2) && p2Positions.Contains(3))
+				{
+					p2Points++;
+					p1Positions.Clear();
+					p2Positions.Clear();
+					return true;
+				}
+				if (p2Positions.Contains(4) && p2Positions.Contains(5) && p2Positions.Contains(6))
+				{
+					p2Points++;
+					p1Positions.Clear();
+					p2Positions.Clear();
+					return true;
+				}
+				if (p2Positions.Contains(7) && p2Positions.Contains(8) && p2Positions.Contains(9))
+				{
+					p2Points++;
+					p1Positions.Clear();
+					p2Positions.Clear();
+					return true;
+				}
+				if (p2Positions.Contains(1) && p2Positions.Contains(4) && p2Positions.Contains(7))
+				{
+					p2Points++;
+					p1Positions.Clear();
+					p2Positions.Clear();
+					return true;
+				}
+				if (p2Positions.Contains(2) && p2Positions.Contains(5) && p2Positions.Contains(8))
+				{
+					p2Points++;
+					p1Positions.Clear();
+					p2Positions.Clear();
+					return true;
+				}
+				if (p2Positions.Contains(3) && p2Positions.Contains(6) && p2Positions.Contains(9))
+				{
+					p2Points++;
+					p1Positions.Clear();
+					p2Positions.Clear();
+					return true;
+				}
+				if (p2Positions.Contains(1) && p2Positions.Contains(5) && p2Positions.Contains(9))
+				{
+					p2Points++;
+					p1Positions.Clear();
+					p2Positions.Clear();
+					return true;
+				}
+				if (p2Positions.Contains(7) && p2Positions.Contains(5) && p2Positions.Contains(3))
+				{
+					p2Points++;
+					p1Positions.Clear();
+					p2Positions.Clear();
+					return true;
+				}
 
 
 
 				if (p2Positions.Count + p1Positions.Count == 9)
-					return "it's a tie";
-				else
-					return "nobody wins!";
+				{
+					p1Positions.Clear();
+					p2Positions.Clear();
+					return true;
+				}
+				else return true;
+				
+
+				
 			}
-			else return "This position is taken";
+			else return false; //pozycja zajeta ruch niedozwolony
 
 
 
