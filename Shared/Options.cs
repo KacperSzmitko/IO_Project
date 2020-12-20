ce1b61affc330421c1751efbb2eeb760f698d95a
Marcin Tajsner <marcin.tajsner@gmail.com>
2020-12-20 14:29:28 +0100

    Nowa opcja komunikacji z serwerem

--- Shared/Options.cs
+++ Shared/Options.cs
@@ -3,20 +3,21 @@ namespace Shared
 {
     public enum Options
     {
-        //                          CLIENT_FIELDS                                 SERVER RESPONSE
-        LOGOUT = 0, //Ready         SessionID:<>$$                                Error:<>$$
-        MATCH_HISTORY = 1, //Ready  SessionID:<>$$                                Error:<>$$DATA:<xmlDoc>$$
-        RANK = 2, //Ready           SessionID:<>$$                                Error:<>$$DATA:<xmlDoc>$$
-        SEARCH_GAME = 3, //Ready    SessionID:<>$$                                Error:<>$$
-        END_GAME = 4,//             SessionID:<>$$                                Error:<>$$PlayerElo:<>$$OppElo:<>$$ 
-        LOGIN = 5, //Ready          Username:<>$$Password:<>$$                    Error:<>$$SessionID:<>$$Elo:<>$$
-        CREATE_USER = 6, //Ready    Username:<>$$Password:<>$$                    Error:<>$$
-        SEND_MOVE = 7, //           SessionID:<>$$Move:<>$$                       Error:<>$$Score:np.1-0$$ gdzie pierwsza jest klienta ktroy wyslal ruch
-        DISCONNECT = 8, //Ready                                                   Error:<>$$
-        CHECK_USER_NAME = 9, //     Username:<>$$                                 Error:<>$$
-        SEND_MATCH = 10, //                                                       Error:<>$$OppName:<>$$OppRank:<>$$IsCross:<>
-        OPP_MOVE = 11,   //                                                       Error:<>$$Score:<>$$OppMove:<>$$
-                                                                                             
+                              //    ADDITIONAl CLIENT FIELDS                      SERVER RESPONSE
+        LOGOUT = 0,           //    sessionid:<>$$                                Error:<>$$
+        MATCH_HISTORY = 1,    //    sessionid:<>$$                                Error:<>$$DATA:<xmlDoc>$$
+        RANK = 2,             //    sessionid:<>$$                                Error:<>$$DATA:<xmlDoc>$$
+        SEARCH_GAME = 3,      //    sessionid:<>$$                                Error:<>$$
+        END_GAME = 4,         //    sessionid:<>$$                                Error:<>$$PlayerElo:<>$$OppElo:<>$$ 
+        LOGIN = 5,            //    username:<>$$password:<>$$                    Error:<>$$SessionID:<>$$Elo:<>$$
+        CREATE_USER = 6,      //    username:<>$$password:<>$$                    Error:<>$$
+        SEND_MOVE = 7,        //    sessionid:<>$$move:<>$$                       Error:<>$$Score:<>$$ np. 1-0 gdzie pierwsza jest klienta ktroy wyslal ruch
+        DISCONNECT = 8,       //                                                  Error:<>$$
+        CHECK_USER_NAME = 9,  //    username:<>$$                                 Error:<>$$
+        SEND_MATCH = 10,      //                                                  Error:<>$$OppName:<>$$OppRank:<>$$IsCross:<>
+        OPP_MOVE = 11,        //                                                  Error:<>$$Score:<>$$OppMove:<>$$
+        STOP_SEARCHING = 12,  //                                                  Error:<>$$    
+        
     }
 
 
